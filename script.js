4928fe56aeaf2796d0574c48090501bfe0db5d4c
kgaugelomokgalaka <kgaugelohelen06@gmail.com>
2024-11-07 09:48:33 +0200

    add public folder

--- script.js
+++ script.js
@@ -47,3 +47,19 @@ taskInput.addEventListener('keypress', (event) => {
         addTask();
     }
 });
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'TO-DO-LIST.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
